0f80fb312b8f4d27f1c65434e017e6a71b9485a5
ANKIT KUMAR YADAV <78925345+99003648@users.noreply.github.com>
2021-02-15 15:59:13 +0530

    Update String_Handling.py

--- Assignment_3/String_Handling.py
+++ Assignment_3/String_Handling.py
@@ -1,4 +1,4 @@
-#a,b,c=input("Enter Malformed Time String: ").split(":")
+#Correct the malformed time string , for e.g "5:70:65" to "6:11:05"
 def func(a,b,c):
     
     if int(c)>59:
@@ -29,8 +29,7 @@ def func(a,b,c):
     elif int(a)>12:
         a=int(a)%12    
     return (a,b,c)
-
-
+#Correct the malformed date string , for e.g. "45/8/2018" to "14/9/2018"
 x=[1,3,5,7,8,10,12]
 z=[4,6,9,11]
 def func(a,b,c):
@@ -83,6 +82,7 @@ def func(a,b,c):
                 b=int(b)+mon
     return (a,b,c)
 
+#Convert ip address from "a.b.c.d" format into integer and vice versa
 import ipaddress
 def ipaddress_2(x):
     try:
@@ -91,13 +91,17 @@ def ipaddress_2(x):
     except:
         addr1 = ipaddress.ip_address(x)  
         return(int(addr1))
-
+    
+#Check whether given string is isogram or not
 def isogram(x):
     print(True if len(list(x))==len(set(x)) else False)
-
+    
+#Given a string, find the mexican wave
 x=list('ankt')
 def mexican(temp):
     temp=[print("".join(x[:i])+"".join(x[i].upper())+"".join(x[i+1:])) for i in range(len(x))]
+
+#Given a number, find the largest number by deleting single digit (order of digits will remain same)
 
 def Deleting_singal_digit(x):
     z=0
@@ -109,6 +113,8 @@ def Deleting_singal_digit(x):
         lis_t.append(int("".join(lis_t_2)))
         z+=1
     print(max(lis_t))
+ 
+#Given a number, find the largest number by shuffling the digits
 
 from itertools import permutations
 def pert(x):
@@ -117,7 +123,7 @@ def pert(x):
     for i in t:
         lis_t.append(int("".join(i)))
     print(max(lis_t))
-
+#Compute the word frequency in given message
 
 def Highest_num_suffule(x):
     dic_t={}
@@ -127,11 +133,12 @@ def Highest_num_suffule(x):
         else:
             continue
     print(dic_t)
-
+    
+#Generate accumulated strings,e.g. abcd ==> A-Bb-Ccc-Dddd
 def accumulated_strings(x):
     for i in x:
         print(i.upper()+i*x.index(i))
-
+#RGB to Hex conversion and vice versa, e.g. (255,0,255) into 0xFF00FF
 def rgb_hex(x):
     print(x)
     if 'x' in x[0]:
